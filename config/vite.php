2924e4efb8d43605dd5a0da67690937a51e7d2ab
ssebin <96987066+ssebin@users.noreply.github.com>
2025-01-15 18:24:07 +0800

    feat: add Vite configuration file to define build path

---
+++ config/vite.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Vite Build Path
+     |--------------------------------------------------------------------------
+     |
+     | This option defines the path to the Vite build directory, relative to
+     | the public directory. By default, Vite places its files in the
+     | "public/build" directory. Update this if necessary.
+     |
+     */
+    'build_path' => 'build',
+];
